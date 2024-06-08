ce44f7afc85aaebefe61102ce812bb3ded5242c4
jesus0207 <pena47337@gmail.com>
2024-06-08 13:36:21 -0500

    Update permissions.py

--- Course/permissions.py
+++ Course/permissions.py
@@ -18,8 +18,9 @@ class IsCoursePermission(permissions.BasePermission):
             bool: True if the requesting user is the owner of the object, False otherwise.
         """
         return obj.instructor.id == request.user.id
-    
-class IsYourOwnId(permissions.BasePermission):
+
+
+class IsYourOwnIdInstructor(permissions.BasePermission):
     """
     Permission class to check if the requesting user is the same of the body user_id.
     """
@@ -35,3 +36,21 @@ class IsYourOwnId(permissions.BasePermission):
             bool: True if the requesting user is the same of the body user_id, False otherwise.
         """
         return request.user.id == request.data.get('instructor')
+
+
+class IsYourOwnIdStudent(permissions.BasePermission):
+    """
+    Permission class to check if the requesting user is the same of the body user_id.
+    """
+    def has_permission(self, request, view):
+        """
+        Method to determine if the requesting user has permission to create an object with the body user_id.
+
+        Args:
+            request (HttpRequest): The request object.
+            view (APIView): The view instance associated with the request.
+
+        Returns:
+            bool: True if the requesting user is the same of the body user_id, False otherwise.
+        """
+        return request.user.id == request.data.get('student')
