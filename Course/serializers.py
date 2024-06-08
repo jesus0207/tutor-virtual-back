5a077ffac2fda4b34380eb9daefa059d00bd3e5e
jesus0207 <pena47337@gmail.com>
2024-06-08 13:36:50 -0500

    Update serializers.py

--- Course/serializers.py
+++ Course/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from Users.models import User
-from .models import Course
+from .models import Course, FavoriteCourse
 
 
 class BaseCourseSerializer(serializers.ModelSerializer):
@@ -13,7 +13,7 @@ class BaseCourseSerializer(serializers.ModelSerializer):
         Validate that the instructor is a professor.
 
         Args:
-            value (int): The ID of the instructor.
+            value (User): The instructor.
 
         Returns:
             User: The validated instructor.
@@ -22,8 +22,7 @@ class BaseCourseSerializer(serializers.ModelSerializer):
             serializers.ValidationError: If the instructor does not exist or is not a professor.
         """
         try:
-            user = User.objects.get(pk=value.id)
-            if user.rol != "Profesor":
+            if value.rol != "Profesor":
                 raise serializers.ValidationError('Only professors can create courses.')
         except User.DoesNotExist:
             raise serializers.ValidationError('User does not exist.')
@@ -97,3 +96,105 @@ class CourseListSerializer(serializers.ModelSerializer):
             str: The full name of the instructor.
         """
         return f"{obj.instructor.first_name} {obj.instructor.last_name}"
+
+
+class BaseFavoriteCourseSerializer(serializers.ModelSerializer):
+    """
+    Base serializer for FavoriteCourse with common validation.
+    """
+    class Meta:
+        model = FavoriteCourse
+        fields = "__all__"
+        extra_kwargs = {
+                "student": {"required": True},
+                "course": {"required": True},
+                "active": {"required": False}
+            }
+        
+    def validate_student(self, value):
+        """
+        Validate that the user is a student.
+
+        Args:
+            value (User): The student.
+
+        Returns:
+            User: The validated user.
+
+        Raises:
+            serializers.ValidationError: If the user does not exist or is not a student.
+        """
+        try:
+            if value.rol != "Estudiante":
+                raise serializers.ValidationError('Only students can add courses to favorite courses.')
+        except User.DoesNotExist:
+            raise serializers.ValidationError('User does not exist.')
+        return value
+
+    def validate_course(self, value):
+        """
+        Validate that the course is active.
+
+        Args:
+            value (Course): The course.
+
+        Returns:
+            User: The validated course.
+
+        Raises:
+            serializers.ValidationError: If the course is not active.
+        """
+        try:
+            if value.active == False:
+                raise serializers.ValidationError('The course is not active')
+        except User.DoesNotExist:
+            raise serializers.ValidationError('User course not exist.')
+        return value
+
+
+class AddFavoriteCourseSerializer(BaseFavoriteCourseSerializer):
+    """
+    Serializer for adding a course to favorite courses.
+    """
+    def create(self, validated_data):
+        student = validated_data.get("student")
+        course = validated_data.get("course")
+        favorite = FavoriteCourse.create(student=student, course=course)
+        return favorite
+
+
+class DeleteFavoriteCourseSerializer(BaseFavoriteCourseSerializer):
+    """
+    Serializer for deleting a course from favorite courses.
+    """
+    def deactivate_favorite_course(self, student, course):
+        """
+        Deactivate a favorite course by setting its 'active' field to False.
+
+        Args:
+            student (User): The student.
+            course (Course): The course.
+
+        Returns:
+            FavoriteCourse: The updated FavoriteCourse instance or None if not found.
+
+        Raises:
+            serializers.ValidationError: If the FavoriteCourse does not exist.
+        """
+        try:
+            favorite_course = FavoriteCourse.objects.get(student=student, course=course)
+            favorite_course.active = False
+            favorite_course.save()
+            return favorite_course
+        except FavoriteCourse.DoesNotExist:
+            raise serializers.ValidationError('FavoriteCourse does not exist.')
+
+
+class ListFavoriteCourseSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(source = 'course.name', read_only=True)
+    instructor = serializers.CharField(source = 'course.instructor', read_only=True)
+    description = serializers.CharField(source = 'course.description', read_only=True)
+
+    class Meta:
+        model = FavoriteCourse
+        fields = ["student","course","name","instructor", "description", "active"]
