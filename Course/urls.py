0922c76f958460d45c1afe720fd3adb4f21fce61
jesus0207 <pena47337@gmail.com>
2024-06-08 13:37:24 -0500

    Update urls.py

--- Course/urls.py
+++ Course/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('instructor/delete/<int:pk>', DeleteCourseView.as_view(), name='instructor_delete'),
 
     path('student/<int:pk>/chat', Chat.as_view(), name='chat'),
-    path('student/list', List.as_view(), name='student_list')
+    path('student/list', List.as_view(), name='student_list'),
+    path('student/course/favorites/add', AddCourseFavoriteView.as_view(), name='student_courses_favorites_add'),
+    path('student/course/favorites/delete', DeleteFavoriteCourseView.as_view(), name='student_courses_favorites_delete'),
+    path('student/course/favorites/list', ListFavoriteCourseView.as_view(), name='student_courses_favorites_list'),
 ]
